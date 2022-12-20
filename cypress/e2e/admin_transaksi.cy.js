f54fcb11ac57b7639969328cbc78d6f9b0bf1c65
AnnisaFitry <2041720123@student.polinema.ac.id>
2022-12-20 20:39:33 +0700

    update2

--- cypress/e2e/admin_transaksi.cy.js
+++ cypress/e2e/admin_transaksi.cy.js
@@ -1,8 +1,8 @@
 describe('Cek Fungsi Transaksi Admin', () => {
     it('Tambah Transaksi dengan benar', () => {
         cy.visit('http://127.0.0.1:8000')
-        cy.get('input[id="email"]').type("cyntiapd")
-        cy.get('input[id="password"]').type("user123")
+        cy.get('input[id="email"]').type("adminsmk")
+        cy.get('input[id="password"]').type("admin123")
         cy.get('button[type="submit"]').click()
         cy.visit('http://127.0.0.1:8000/transaksi')
         cy.visit('http://127.0.0.1:8000/transaksi/create')
@@ -17,8 +17,8 @@ describe('Cek Fungsi Transaksi Admin', () => {
     });
     it('Tambah Transaksi dengan salah, tanpa cari buku dan anggota', () => {
         cy.visit('http://127.0.0.1:8000')
-        cy.get('input[id="email"]').type("cyntiapd")
-        cy.get('input[id="password"]').type("user123")
+        cy.get('input[id="email"]').type("adminsmk")
+        cy.get('input[id="password"]').type("admin123")
         cy.get('button[type="submit"]').click()
         cy.visit('http://127.0.0.1:8000/transaksi')
         cy.visit('http://127.0.0.1:8000/transaksi/create')
@@ -29,24 +29,24 @@ describe('Cek Fungsi Transaksi Admin', () => {
     });
     it('Detail Transaksi', () => {
         cy.visit('http://127.0.0.1:8000')
-        cy.get('input[id="email"]').type("cyntiapd")
-        cy.get('input[id="password"]').type("user123")
+        cy.get('input[id="email"]').type("adminsmk")
+        cy.get('input[id="password"]').type("admin123")
         cy.get('button[type="submit"]').click()
         cy.visit('http://127.0.0.1:8000/transaksi')
         cy.visit('http://127.0.0.1:8000/transaksi/9')
       });
     it('Update status pengembalian Buku', () => {
         cy.visit('http://127.0.0.1:8000')
-        cy.get('input[id="email"]').type("cyntiapd")
-        cy.get('input[id="password"]').type("user123")
+        cy.get('input[id="email"]').type("adminsmk")
+        cy.get('input[id="password"]').type("admin123")
         cy.get('button[type="submit"]').click()
         cy.visit('http://127.0.0.1:8000/transaksi')
         cy.get(':nth-child(2) > :nth-child(7) > .btn-group > .dropdown-menu > [enctype="multipart/form-data"] > .dropdown-item').click()
     });
     it('Delete Transaksi', () => {
         cy.visit('http://127.0.0.1:8000')
-        cy.get('input[id="email"]').type("cyntiapd")
-        cy.get('input[id="password"]').type("user123")
+        cy.get('input[id="email"]').type("adminsmk")
+        cy.get('input[id="password"]').type("admin123")
         cy.get('button[type="submit"]').click()
         cy.visit('http://127.0.0.1:8000/transaksi')
         cy.get(':nth-child(1) > :nth-child(7) > .btn-group > .dropdown-menu > .pull-left > .dropdown-item').click()
